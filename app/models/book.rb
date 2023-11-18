735b4a83deb41370759666d826407d47166ef913
hidakashun <hidakashun60@gmail.com>
2023-11-18 03:10:25 +0000

    [start]8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -31,6 +31,16 @@ class Book < ApplicationRecord
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
   #ここまで
 
+  #ここからchart.jsを使って過去7日分の投稿数をグラフ化する
+  #scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  #scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+  #ここまで
+
   #検索機能、条件分岐
   def self.search_for(content, method)
     if method == 'perfect'
